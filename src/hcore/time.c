1d946211d0f6830767a05765d52a8774bf9fa2fa
idafi <none>
2017-10-27 12:09:15 -0500

    hcore - simplify time system

--- src/hcore/time.c
+++ src/hcore/time.c
@@ -1,27 +1,13 @@
 #include "time.h"
 
-intern time_state currentState;
+HEXPORT(uint32) Time_GetTicks()
+{
+	return SDL_GetTicks();
+}
 
 HEXPORT(void) Time_Delay(uint32 ms)
 {
 	SDL_Delay(ms);
-}
-
-HEXPORT(void) Time_SetTargetFrametime(uint32 ms)
-{
-	currentState.targetFrametime = ms;
-}
-
-HEXPORT(void) Time_DelayToTarget()
-{
-	if(currentState.targetFrametime > 0)
-	{
-		int target = (int)(currentState.targetFrametime);
-		int diff = target - (int)(currentState.deltaTicks);
-
-		if(diff > 0)
-		{ Time_Delay((uint32)(diff)); }
-	}
 }
 
 HEXPORT(double) Time_TimeProcedure(void(*proc)())
@@ -40,17 +26,4 @@ HEXPORT(double) Time_TimeProcedure(void(*proc)())
 
 	LogError("couldn't time procedure: procedure is null");
 	return 0;
-}
-
-HEXPORT(void) Time_GetSnapshot(time_state *state)
-{
-	uint32 newTicks = SDL_GetTicks();
-
-	currentState = (time_state)
-	{
-		.deltaTicks = newTicks - currentState.totalTicks,
-		.totalTicks = newTicks,
-
-		.targetFrametime = currentState.targetFrametime
-	};
 }
--- src/hcore/time.h
+++ src/hcore/time.h
@@ -2,19 +2,7 @@
 
 #include "_shared.h"
 
+HEXPORT(uint32) Time_GetTicks();
 HEXPORT(void) Time_Delay(uint32 ms);
 
-HEXPORT(void) Time_SetTargetFrametime(uint32 ms);
-HEXPORT(void) Time_DelayToTarget();
-
 HEXPORT(double) Time_TimeProcedure(void(*proc)());
-
-typedef struct
-{
-	uint32 totalTicks;
-	uint32 deltaTicks;
-
-	uint32 targetFrametime;
-} time_state;
-
-HEXPORT(void) Time_GetSnapshot(time_state *state);
