25bd809508cb4ff2cc9233fcb5645f0e0c60b649
idafi <none>
2017-10-31 12:31:48 -0500

    heng - add doc to CoreConfig.AudioConfig

--- src/heng/Core/CoreConfig.cs
+++ src/heng/Core/CoreConfig.cs
@@ -27,28 +27,73 @@ namespace heng
 			public LogLevel MinLevelFile;
 		};
 
+		/// <summary>
+		/// Configuration settings for the engine core's audio system.
+		/// </summary>
 		[StructLayout(LayoutKind.Sequential)]
 		public struct AudioConfig
 		{
+			/// <summary>
+			/// Configuration settings for the audio system's sound manager.
+			/// </summary>
 			[StructLayout(LayoutKind.Sequential)]
 			public struct SoundsConfig
 			{
+				/// <summary>
+				/// The maximum number of sound resources that can be loaded.
+				/// </summary>
 				public int MaxSounds;
 			};
 
+			/// <summary>
+			/// Configuration settings for the audio system's mixer.
+			/// </summary>
 			[StructLayout(LayoutKind.Sequential)]
 			public struct MixerConfig
 			{
+				/// <summary>
+				/// The number of available mixing channels.
+				/// </summary>
 				public int ChannelCount;
+
+				/// <summary>
+				/// The minimum distance of a sound source before attenuation will apply.
+				/// </summary>
 				public int AttenuationThreshold;
+
+				/// <summary>
+				/// The exponent describing the speed with which multi-channel sounds will converge into a single channel.
+				/// <para>I.e., a stereo sound will play from both channels when on top of the listener, but will converge
+				/// into a mono-channel sound the farther away from the listener it goes.</para>
+				/// </summary>
 				public float StereoFalloffExponent;
 			};
 
+			/// <summary>
+			/// The sample format at which audio should be played.
+			/// <para>Sound resources in different formats will be converted to this target format when loaded.</para>
+			/// </summary>
 			public AudioFormat Format;
+
+			/// <summary>
+			/// The sample rate at which audio should be played.
+			/// <para>Sound resources at different sample rates will be converted to the target rate when loaded.</para>
+			/// </summary>
 			public int SampleRate;
+
+			/// <summary>
+			/// The number of channels available to audio output.
+			/// </summary>
 			public int Channels;
 			
+			/// <summary>
+			/// Configuration settings for the audio system's sound manager.
+			/// </summary>
 			public SoundsConfig Sounds;
+
+			/// <summary>
+			/// Configuration settings for the audio system's mixer.
+			/// </summary>
 			public MixerConfig Mixer;
 		};
 
