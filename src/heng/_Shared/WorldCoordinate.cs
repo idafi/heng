49dcf174ed923f0769158a7873ffbad0a3ca07d2
idafi <none>
2017-11-05 21:33:43 -0600

    heng - add WorldPoint (virtualized coordinates)

---
+++ src/heng/_Shared/WorldCoordinate.cs
@@ -0,0 +1,162 @@
+﻿namespace heng
+{
+	/// <summary>
+	/// A virtualized coordinate position.
+	/// <para>A WorldCoordinate is represented as an integer-based "sector," and a 0-1 fractional "subposition"
+	/// within that sector.</para>
+	/// A sector's actual size is determined by the <see cref="PixelsPerSector"/> constant.
+	/// </summary>
+	public struct WorldCoordinate
+	{
+		public const int PixelsPerSector = 200;
+
+		/// <summary>
+		/// This <see cref="WorldCoordinate"/>'s sector position.
+		/// </summary>
+		public readonly int Sector;
+
+		/// <summary>
+		/// The fractional subposition inside this <see cref="WorldCoordinate"/>'s sector.
+		/// </summary>
+		public readonly float Subposition;
+
+		/// <summary>
+		/// Shortcut for new WorldCoordinate(0, 0).
+		/// </summary>
+		public static WorldCoordinate Zero => new WorldCoordinate(0, 0);
+
+		/// <summary>
+		/// Constructs a new WorldCoordinate at the given position.
+		/// <para>If clampSubposition is false, subposition values greater than 1 will be added to the
+		/// new <see cref="WorldCoordinate"/>'s sector position.</para>
+		/// For instance, setting the sector to 1 and subposition 1.5 would place the new <see cref="WorldCoordinate"/>
+		/// at sector 2, subposition 0.5.
+		/// </summary>
+		/// <param name="sector">The new <see cref="WorldCoordinate"/>'s sector position.</param>
+		/// <param name="subposition">The new <see cref="WorldCoordinate"/>'s subposition within its sector.</param>
+		/// <param name="clampSubposition">Whether to clamp subposition between 0 and 1, or add excess
+		/// position to the sector value.</param>
+		public WorldCoordinate(int sector, float subposition, bool clampSubposition = true)
+		{
+			if(clampSubposition)
+			{
+				Sector = sector;
+				Subposition = HMath.Clamp(subposition, 0, 1 - float.Epsilon);
+			}
+			else
+			{	
+				int diff = HMath.FloorToInt(subposition);
+
+				Sector = sector + diff;
+				Subposition = subposition - diff;
+			}
+		}
+
+		/// <inheritdoc />
+		public static WorldCoordinate operator +(WorldCoordinate a, WorldCoordinate b)
+		{
+			int sec = a.Sector + b.Sector;
+			float sub = a.Subposition + b.Subposition;
+			
+			return new WorldCoordinate(sec, sub, false);
+		}
+		
+		/// <inheritdoc />
+		public static WorldCoordinate operator -(WorldCoordinate a, WorldCoordinate b)
+		{
+			int sec = a.Sector - b.Sector;
+			float sub = a.Subposition - b.Subposition;
+			
+			return new WorldCoordinate(sec, sub, false);
+		}
+
+		/// <inheritdoc />
+		public static bool operator ==(WorldCoordinate a, WorldCoordinate b)
+		{
+			return (a.Sector == b.Sector) && (a.Subposition == b.Subposition);
+		}
+		
+		/// <inheritdoc />
+		public static bool operator !=(WorldCoordinate a, WorldCoordinate b)
+		{
+			return (a.Sector != b.Sector) || (a.Subposition != b.Subposition);
+		}
+		
+		/// <inheritdoc />
+		public static bool operator >(WorldCoordinate a, WorldCoordinate b)
+		{
+			return (a.Sector == b.Sector) ? (a.Subposition > b.Subposition) : (a.Sector > b.Sector);
+		}
+		
+		/// <inheritdoc />
+		public static bool operator <(WorldCoordinate a, WorldCoordinate b)
+		{
+			return (a.Sector == b.Sector) ? (a.Subposition < b.Subposition) : (a.Sector < b.Sector);
+		}
+
+		/// <summary>
+		/// Linearly interpolates between two <see cref="WorldCoordinate"/>s.
+		/// </summary>
+		/// <param name="a">The interpolation start point.</param>
+		/// <param name="b">The interpolation end point.</param>
+		/// <param name="t">A 0-1 fraction, indicating which value between the two points to return.</param>
+		/// <returns>A <see cref="WorldCoordinate"/> value between the start and end point.</returns>
+		public static WorldCoordinate Lerp(WorldCoordinate a, WorldCoordinate b, float t)
+		{
+			WorldCoordinate diff = b - a;
+			
+			float secAdd = diff.Sector * t;
+			float subAdd = diff.Subposition * t;
+			float secRem = secAdd % 1;
+			
+			int sec = a.Sector + HMath.FloorToInt(secAdd);
+			float sub = a.Subposition + subAdd + secRem;
+			return new WorldCoordinate(sec, sub, false);
+		}
+
+		/// <inheritdoc />
+		public override bool Equals(object obj) => base.Equals(obj);
+
+		/// <inheritdoc />
+		public override int GetHashCode() => base.GetHashCode();
+
+		/// <inheritdoc />
+		public override string ToString() => $"{Sector}{Subposition:.#########}";
+
+		/// <summary>
+		/// Clamps this <see cref="WorldCoordinate"/> between two other <see cref="WorldCoordinate"/> values.
+		/// </summary>
+		/// <param name="lower">The lower bound to clamp between.</param>
+		/// <param name="upper">The upper bound to clamp between.</param>
+		/// <returns>A new <see cref="WorldCoordinate"/>, clamped within the given bounds.</returns>
+		public WorldCoordinate Clamp(WorldCoordinate lower, WorldCoordinate upper)
+		{
+			return (this < lower) ? lower : (this > upper) ? upper : this;
+		}
+
+		/// <summary>
+		/// Translates this <see cref="WorldCoordinate"/> by the given number of pixels.
+		/// </summary>
+		/// <param name="dist">The amount of pixels by which to translate this <see cref="WorldCoordinate"/>.</param>
+		/// <returns>A new <see cref="WorldCoordinate"/>, translated by the given pixel distance.</returns>
+		public WorldCoordinate PixelTranslate(float dist)
+		{
+			dist /= PixelsPerSector;
+			return new WorldCoordinate(Sector, Subposition + dist, false);
+		}
+		
+		/// <summary>
+		/// Returns the distance, in pixels, between this <see cref="WorldCoordinate"/> and another.
+		/// </summary>
+		/// <param name="other">The other <see cref="WorldCoordinate"/>.</param>
+		/// <returns>The distance, in pixels, between the two <see cref="WorldCoordinate"/>s.</returns>
+		public float PixelDistance(WorldCoordinate other)
+		{
+			int secDiff = this.Sector - other.Sector;
+			float subDiff = this.Subposition - other.Subposition;
+			float net = (secDiff + subDiff) * PixelsPerSector;
+			
+			return net;
+		}
+	};
+}
---
+++ src/heng/_Shared/WorldPoint.cs
@@ -0,0 +1,165 @@
+﻿namespace heng
+{
+	/// <summary>
+	/// A position in 2D space using virtualized <see cref="WorldCoordinate"/>s.
+	/// <para>A WorldPoint is represented as an integer-based "sector" position, and a 0-1 fractional
+	/// "subposition" within that sector.</para>
+	/// A sector's actual size is determined by the <see cref="WorldCoordinate.PixelsPerSector"/> constant.
+	/// <para>WorldPoints can interact with pixel-space  <see cref="Vector2"/> values using
+	/// <see cref="PixelDistance(WorldPoint)"/> and <see cref="PixelTranslate(Vector2)"/></para>
+	/// </summary>
+	public struct WorldPoint
+	{
+		/// <summary>
+		/// This WorldPoint's X coordinate.
+		/// </summary>
+		public readonly WorldCoordinate X;
+
+		/// <summary>
+		/// This WorldPoint's Y coordinate.
+		/// </summary>
+		public readonly WorldCoordinate Y;
+		
+		/// <summary>
+		/// Shorctcut for a (0, 0), (0, 0) <see cref="WorldPoint"/>.
+		/// </summary>
+		public static WorldPoint Zero => new WorldPoint(WorldCoordinate.Zero, WorldCoordinate.Zero);
+
+		/// <summary>
+		/// This WorldPoint's Sector position, as a <see cref="Vector2"/>.
+		/// </summary>
+		public Vector2 Sector => new Vector2(X.Sector, Y.Sector);
+		
+		/// <summary>
+		/// This WorldPoint's Subposition, as a <see cref="Vector2"/>.
+		/// </summary>
+		public Vector2 Subposition => new Vector2(X.Subposition, Y.Subposition);
+		
+		/// <summary>
+		/// Constructs a new <see cref="WorldPoint"/> using the given <see cref="WorldCoordinate"/>s.
+		/// </summary>
+		/// <param name="x">The new <see cref="WorldPoint"/>'s X coordinate.</param>
+		/// <param name="y">The new <see cref="WorldPoint"/>'s Y coordinate.</param>
+		public WorldPoint(WorldCoordinate x, WorldCoordinate y)
+		{
+			X = x;
+			Y = y;
+		}
+		
+		/// <summary>
+		/// Construct a new <see cref="WorldPoint"/> at the given sector and subposition.
+		/// </summary>
+		/// <param name="sectorX">The new <see cref="WorldPoint"/>'s X sector coordinate.</param>
+		/// <param name="sectorY">The new <see cref="WorldPoint"/>'s Y sector coordinate.</param>
+		/// <param name="subposition">The new <see cref="WorldPoint"/>'s subposition coordinates.</param>
+		public WorldPoint(int sectorX, int sectorY, Vector2 subposition)
+		{
+			X = new WorldCoordinate(sectorX, subposition.X);
+			Y = new WorldCoordinate(sectorY, subposition.Y);
+		}
+		
+		/// <summary>
+		/// Construct a new <see cref="WorldPoint"/> at the given sector and subposition.
+		/// </summary>
+		/// <param name="sector">The new <see cref="WorldPoint"/>'s sector coordinates.</param>
+		/// <param name="subposition">The new <see cref="WorldPoint"/>'s subposition coordinates.</param>
+		public WorldPoint(Vector2 sector, Vector2 subposition)
+		{
+			X = new WorldCoordinate(HMath.FloorToInt(sector.X), subposition.X);
+			Y = new WorldCoordinate(HMath.FloorToInt(sector.Y), subposition.Y);
+		}
+
+		/// <inheritdoc />
+		public static WorldPoint operator +(WorldPoint a, WorldPoint b)
+			=> new WorldPoint(a.X + b.X, a.Y + b.Y);
+
+		/// <inheritdoc />
+		public static WorldPoint operator -(WorldPoint a, WorldPoint b)
+			=> new WorldPoint(a.X - b.X, a.Y - b.Y);
+		
+		/// <inheritdoc />
+		public static bool operator ==(WorldPoint a, WorldPoint b)
+			=> (a.X == b.X) && (a.Y == b.Y);
+		
+		/// <inheritdoc />
+		public static bool operator !=(WorldPoint a, WorldPoint b)
+			=> (a.X != b.X) || (a.Y != b.Y);
+
+		/// <summary>
+		/// Linearly interpolates between two <see cref="WorldPoint"/>s.
+		/// </summary>
+		/// <param name="a">The interpolation start point.</param>
+		/// <param name="b">The interpolation end point.</param>
+		/// <param name="t">A 0-1 fraction, indicating which value between the two points to return.</param>
+		/// <returns>A <see cref="WorldPoint"/> value between the start and end point.</returns>
+		public static WorldPoint Lerp(WorldPoint a, WorldPoint b, float t)
+		{
+			WorldCoordinate x = WorldCoordinate.Lerp(a.X, b.X, t);
+			WorldCoordinate y = WorldCoordinate.Lerp(a.Y, b.Y, t);
+			
+			return new WorldPoint(x, y);
+		}
+
+		/// <inheritdoc />
+		public override bool Equals(object obj) => base.Equals(obj);
+
+		/// <inheritdoc />
+		public override int GetHashCode() => base.GetHashCode();
+
+		/// <inheritdoc />
+		public override string ToString() => $"({X.Sector}, {Y.Sector}), ({X.Subposition}, {Y.Subposition})";
+
+		/// <summary>
+		/// Clamps this <see cref="WorldPoint"/> between two other <see cref="WorldPoint"/> values.
+		/// </summary>
+		/// <param name="lower">The lower bound to clamp between.</param>
+		/// <param name="upper">The upper bound to clamp between.</param>
+		/// <returns>A new <see cref="WorldPoint"/>, clamped within the given bounds.</returns>
+		public WorldPoint Clamp(WorldPoint lower, WorldPoint upper)
+		{
+			WorldCoordinate x = this.X.Clamp(lower.X, upper.X);
+			WorldCoordinate y = this.Y.Clamp(lower.Y, upper.Y);
+			
+			return new WorldPoint(x, y);
+		}
+		
+		/// <summary>
+		/// Translates this <see cref="WorldPoint"/> by the given pixel-space vector..
+		/// </summary>
+		/// <param name="dist">The pixel-space vector by which to translate this <see cref="WorldPoint"/>.</param>
+		/// <returns>A new <see cref="WorldPoint"/>, translated by the given pixel-space vector.</returns>
+		public WorldPoint PixelTranslate(Vector2 dist)
+		{
+			WorldCoordinate x = this.X.PixelTranslate(dist.X);
+			WorldCoordinate y = this.Y.PixelTranslate(dist.Y);
+			
+			return new WorldPoint(x, y);
+		}
+		
+		/// <summary>
+		/// Returns a pixel-space vector between this <see cref="WorldPoint"/> and another.
+		/// </summary>
+		/// <param name="other">The other <see cref="WorldPoint"/>.</param>
+		/// <returns>The pixel-space vector between the two <see cref="WorldPoint"/>s.</returns>
+		public Vector2 PixelDistance(WorldPoint other)
+		{
+			float x = this.X.PixelDistance(other.X);
+			float y = this.Y.PixelDistance(other.Y);
+			
+			return new Vector2(x, y);
+		}
+		
+		/// <summary>
+		/// Finds the least common sector between this <see cref="WorldPoint"/> and another.
+		/// </summary>
+		/// <param name="other">The other <see cref="WorldPoint"/>.</param>
+		/// <returns>A new <see cref="WorldPoint"/>, located at the origin of the least common sector.</returns>
+		public WorldPoint LeastCommonSector(WorldPoint other)
+		{
+			int xSec = HMath.Min(this.X.Sector, other.X.Sector);
+			int ySec = HMath.Min(this.Y.Sector, other.Y.Sector);
+
+			return new WorldPoint(new WorldCoordinate(xSec, 0), new WorldCoordinate(ySec, 0));
+		}
+	};
+}
