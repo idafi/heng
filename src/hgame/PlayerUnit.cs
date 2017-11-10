573eff272cc760e659e27d1a37e4f24f2bf515dc
idafi <none>
2017-11-10 14:10:04 -0600

    hgame - move test sound source & playback to Scenery, attach listener to player unit

--- src/hgame/PlayerUnit.cs
+++ src/hgame/PlayerUnit.cs
@@ -1,5 +1,4 @@
 ﻿using heng;
-using heng.Audio;
 using heng.Input;
 using heng.Physics;
 using heng.Video;
@@ -12,36 +11,31 @@ namespace hgame
 		const float speed = 150;
 
 		readonly Texture texture;
-		readonly Sound sound;
 
 		readonly int inputDevice;
 		readonly int rigidBody;
 		readonly int sprite;
-		readonly int soundSource;
 	
 		public PlayerUnit(GamestateBuilder newState)
 		{
 			Assert.Ref(newState);
 
 			texture = new Texture("../../data/textest.bmp");
-			sound = new Sound("../../data/sto.ogg");
 
 			InputDevice device = new InputDevice();
 			device.RemapAxis("Horizontal", new ButtonAxis(new Key(KeyCode.Left), new Key(KeyCode.Right)));
 			device.RemapAxis("Vertical", new ButtonAxis(new Key(KeyCode.Down), new Key(KeyCode.Up)));
-			device.RemapButton("SoundTest", new Key(KeyCode.Space));
 			
 			WorldPoint pos = new WorldPoint(new Vector2(280 - 16, 100 - 16));
 
 			Polygon collider = new Polygon(new Vector2(0, 0), new Vector2(32, 0), new Vector2(32, 32), new Vector2(0, 32));
 			RigidBody body = new RigidBody(pos, new ConvexCollider(collider));
 			Sprite spr = new Sprite(texture, pos, 0);
-			SoundSource src = new SoundSource(body.Position);
 
 			inputDevice = newState.Input.AddDevice(device);
 			rigidBody = newState.Physics.AddRigidBody(body);
 			sprite = newState.Video.AddDrawable(spr);
-			soundSource = newState.Audio.AddSoundSource(src);
+			newState.Audio.SetListenerPosition(body.Position);
 
 			newState.Video.SetCamera(new Camera(body.Position));
 		}
@@ -52,30 +46,20 @@ namespace hgame
 
 			PlayerUnit oldUnit = oldState.PlayerUnit;
 			texture = oldUnit.texture;
-			sound = oldUnit.sound;
 
 			InputDevice device = oldState.Input.Devices[oldUnit.inputDevice];
 			RigidBody body = oldState.Physics.RigidBodies[oldUnit.rigidBody];
 			Sprite spr = (Sprite)(oldState.Video.Drawables[oldUnit.sprite]);
-			SoundSource src = oldState.Audio.SoundSources[oldUnit.soundSource];
-		
+
 			float x = device.GetAxisFrac("Horizontal");
 			float y = device.GetAxisFrac("Vertical");
 			float spd = speed * oldState.Time.Delta;
 			Vector2 move = new Vector2(x, y).ClampMagnitude(0, 1) * spd;
-			
-			if(device.GetButtonPressed("SoundTest"))
-			{
-				SoundInstance instc = new SoundInstance(sound);
-
-				int instcID = newState.Audio.AddSoundInstance(instc);
-				src = src.PlaySound(instc.ID);
-			}
 
 			inputDevice = newState.Input.AddDevice(device);
 			rigidBody = newState.Physics.AddRigidBody(body.AddImpulse(move));
 			sprite = newState.Video.AddDrawable(spr.Reposition(body.Position));
-			soundSource = newState.Audio.AddSoundSource(src.Reposition(body.Position));
+			newState.Audio.SetListenerPosition(body.Position);
 
 			Window window = oldState.Video.Windows[0];
 			Vector2 centerOffset = new Vector2(window.Rect.W / 2, window.Rect.H / 2);
--- src/hgame/Scenery.cs
+++ src/hgame/Scenery.cs
@@ -1,4 +1,6 @@
 ﻿using heng;
+using heng.Audio;
+using heng.Input;
 using heng.Physics;
 using heng.Video;
 
@@ -7,24 +9,34 @@ namespace hgame
 	public class Scenery
 	{
 		readonly Texture texture;
+		readonly Sound sound;
 
+		readonly int inputDevice;
 		readonly int staticBody;
 		readonly int sprite;
+		readonly int soundSource;
 
 		public Scenery(GamestateBuilder newState)
 		{
 			Assert.Ref(newState);
 
 			texture = new Texture("../../data/textest.bmp");
+			sound = new Sound("../../data/sto.ogg");
+
+			InputDevice device = new InputDevice();
+			device.RemapButton("SoundTest", new Key(KeyCode.Space));
 
 			WorldPoint pos = new WorldPoint(new Vector2(320 - 16, 240 - 16));
 
 			Polygon collider = new Polygon(new Vector2(0, 0), new Vector2(32, 0), new Vector2(32, 32), new Vector2(0, 32));
 			StaticBody body = new StaticBody(pos, new ConvexCollider(collider));
 			Sprite spr = new Sprite(texture, pos, 0);
+			SoundSource src = new SoundSource(body.Position);
 
+			inputDevice = newState.Input.AddDevice(device);
 			staticBody = newState.Physics.AddStaticBody(body);
 			sprite = newState.Video.AddDrawable(spr);
+			soundSource = newState.Audio.AddSoundSource(src);
 		}
 
 		Scenery(Gamestate oldState, GamestateBuilder newState)
@@ -33,12 +45,25 @@ namespace hgame
 
 			Scenery oldScenery = oldState.Scenery;
 			texture = oldScenery.texture;
+			sound = oldScenery.sound;
 
+			InputDevice device = oldState.Input.Devices[oldScenery.inputDevice];
 			StaticBody body = oldState.Physics.StaticBodies[oldScenery.staticBody];
 			Sprite spr = (Sprite)(oldState.Video.Drawables[oldScenery.sprite]);
+			SoundSource src = oldState.Audio.SoundSources[oldScenery.soundSource];
 
+			if(device.GetButtonPressed("SoundTest"))
+			{
+				SoundInstance instc = new SoundInstance(sound);
+
+				int instcID = newState.Audio.AddSoundInstance(instc);
+				src = src.PlaySound(instc.ID);
+			}
+
+			inputDevice = newState.Input.AddDevice(device);
 			staticBody = newState.Physics.AddStaticBody(body);
 			sprite = newState.Video.AddDrawable(spr);
+			soundSource = newState.Audio.AddSoundSource(src);
 		}
 
 		public Scenery Update(Gamestate oldState, GamestateBuilder newState)
