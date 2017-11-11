7a82fab78dd52d0fff4987d618a9d4c96f3c7a2e
idafi <none>
2017-11-11 13:11:55 -0600

    heng - clean, reorganize PhysicsState

--- src/heng/Physics/PhysicsState.cs
+++ src/heng/Physics/PhysicsState.cs
@@ -4,73 +4,81 @@ namespace heng.Physics
 {
 	/// <summary>
 	/// Represents an immutable snapshot of the physics system's current state.
-	/// <para>Each frame, the provided <see cref="RigidBody"/> and <see cref="StaticBody"/> instances
-	/// apply any forces they've accumulated, before testing and resolving collisions.</para>
+	/// <para>Each frame, the provided <see cref="IPhysicsObject"/> instances apply
+	/// any forces they've accumulated, before testing and resolving collisions.</para>
 	/// (This is a really pathetic and egregiously inaccurate representation of "physics," but it works for now.)
 	/// </summary>
 	public class PhysicsState
 	{
 		/// <summary>
-		/// All <see cref="RigidBody"/> instances in use.
+		/// All <see cref="IPhysicsObject"/> instances in use.
 		/// </summary>
-		public readonly IReadOnlyList<RigidBody> RigidBodies;
-
-		/// <summary>
-		/// All <see cref="StaticBody"/> instances in use.
-		/// </summary>
-		public readonly IReadOnlyList<StaticBody> StaticBodies;
+		public readonly IReadOnlyList<IPhysicsObject> PhysicsObjects;
 
 		/// <summary>
 		/// Constructs a new <see cref="PhysicsState"/> using the provided physics objects.
-		/// <para>All physics-related translations and calculations will take place during this construction.</para>
-		/// E.g., the <see cref="RigidBody"/> instances that will be present in the <see cref="RigidBodies"/>
+		/// <para>All physics-related transformations and calculations will take place during this construction.</para>
+		/// E.g., the <see cref="RigidBody"/> instances that will be present in the <see cref="PhysicsObjects"/>
 		/// collection will be in a new positon, after all impulses are applied and all collisions are resolved.
 		/// </summary>
-		/// <param name="rigidBodies"></param>
-		/// <param name="staticBodies"></param>
-		public PhysicsState(IEnumerable<RigidBody> rigidBodies, IEnumerable<StaticBody> staticBodies)
+		public PhysicsState(IEnumerable<IPhysicsObject> physicsObjects)
 		{
-			if(rigidBodies != null)
+			PhysicsObjects = AddPhysicsObjects(physicsObjects);
+		}
+
+		IReadOnlyList<IPhysicsObject> AddPhysicsObjects(IEnumerable<IPhysicsObject> objects)
+		{
+			if(objects != null)
 			{
-				if(staticBodies != null)
-				{
-					List<RigidBody> newRigidBodies = new List<RigidBody>();
-					List<StaticBody> newStaticBodies = new List<StaticBody>(staticBodies);
+				List<IPhysicsObject> newObjects = ImpulsePass(objects);
+				IReadOnlyList<IPhysicsObject> finalObjects = CollisionPass(newObjects);
 
-					if(newStaticBodies.RemoveAll(b => b == null) > 0)
-					{ Log.Error("found and removed null StaticBodies while constructing PhysicsState"); }
-
-					foreach(RigidBody b in rigidBodies)
-					{
-						if(b != null)
-						{ newRigidBodies.Add(b.ApplyImpulses()); }
-						else
-						{ Log.Error("couldn't add RigidBody to PhysicsState: body is null"); }
-					}
-
-					List<IPhysicsObject> allObjects = new List<IPhysicsObject>();
-					allObjects.AddRange(newRigidBodies);
-					allObjects.AddRange(newStaticBodies);
-
-					CollisionTester tester = new CollisionTester();
-					foreach(CollisionData collision in tester.GetCollisions(allObjects))
-					{
-						if(collision.Object is RigidBody rb)
-						{
-							// FIXME: slow FIXME FIXME slow FIXME slow slow slow slow slow FIXME
-							int i = newRigidBodies.FindIndex(r => r == rb);
-							newRigidBodies[i] = rb.Translate(collision.MTV);
-						}
-					}
-
-					RigidBodies = newRigidBodies;
-					StaticBodies = newStaticBodies;
-				}
-				else
-				{ Log.Error("couldn't construct PhysicsState: staticBodies collection is null"); }
+				return finalObjects;
 			}
 			else
-			{ Log.Error("couldn't construct PhysicsState: rigidBodies collection is null"); }
+			{ Log.Warning("PhysicsState constructed will null physics objects collection"); }
+
+			return new IPhysicsObject[0];
+		}
+
+		List<IPhysicsObject> ImpulsePass(IEnumerable<IPhysicsObject> objects)
+		{
+			Assert.Ref(objects);
+
+			List<IPhysicsObject> newObjects = new List<IPhysicsObject>();
+
+			foreach(IPhysicsObject obj in objects)
+			{
+				if(obj != null)
+				{
+					if(obj is RigidBody rb)
+					{ newObjects.Add(rb.ApplyImpulses()); }
+					else
+					{ newObjects.Add(obj); }
+				}
+				else
+				{ Log.Error("couldn't add IPhysicsObject to PhysicsState: object is null"); }
+			}
+
+			return newObjects;
+		}
+
+		IReadOnlyList<IPhysicsObject> CollisionPass(List<IPhysicsObject> newObjects)
+		{
+			Assert.Ref(newObjects);
+
+			CollisionTester tester = new CollisionTester();
+			foreach(CollisionData collision in tester.GetCollisions(newObjects))
+			{
+				if(collision.Object is RigidBody rb)
+				{
+					// FIXME: slow FIXME FIXME slow FIXME slow slow slow slow slow FIXME
+					int i = newObjects.FindIndex(r => r == rb);
+					newObjects[i] = rb.Translate(collision.MTV);
+				}
+			}
+
+			return newObjects;
 		}
 	};
 }
