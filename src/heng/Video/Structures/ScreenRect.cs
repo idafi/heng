6c1544aab9a45f7c77fec62accdab571178a866d
idafi <none>
2017-11-11 14:53:12 -0600

    heng - add Rect structure + ScreenRect conversions

--- src/heng/Video/Structures/ScreenRect.cs
+++ src/heng/Video/Structures/ScreenRect.cs
@@ -1,17 +1,21 @@
 ﻿namespace heng.Video
 {
 	/// <summary>
-	/// Represents an integer-based, 2-dimensional pixel area.
+	/// Represents an integer-based, 2-dimensional rectangular pixel area.
+	/// <para>Unlike a plain <see cref="Rect"/>, a <see cref="ScreenRect"/> originates
+	/// from its bottom-left corner, to better parallel other screen-space operations.</para>
+	/// Casting between a <see cref="Rect"/> and a <see cref="ScreenRect"/> will automatically
+	/// account for the difference.
 	/// </summary>
 	public struct ScreenRect
 	{
 		/// <summary>
-		/// The X coordinate of the rect's position.
+		/// The X coordinate of the rect's position (bottom-left corner).
 		/// </summary>
 		public readonly int X;
 
 		/// <summary>
-		/// The Y coordinate of the rect's position.
+		/// The Y coordinate of the rect's position (bottom-left corner).
 		/// </summary>
 		public readonly int Y;
 
@@ -26,7 +30,7 @@
 		public readonly int H;
 
 		/// <summary>
-		/// Returns the rect's position as a <see cref="ScreenPoint"/>.
+		/// Returns the rect's position (bottom-left corner) as a <see cref="ScreenPoint"/>.
 		/// </summary>
 		public ScreenPoint Position => new ScreenPoint(X, Y);
 
@@ -55,6 +59,28 @@
 		public static bool operator !=(ScreenRect a, ScreenRect b)
 		{
 			return (a.X == b.X && a.Y == b.Y && a.W == b.W && a.H == b.H);
+		}
+
+		/// <inheritdoc />
+		public static explicit operator Rect(ScreenRect rect)
+		{
+			Vector2 ext = new Vector2(rect.W / 2, rect.H / 2);
+			Vector2 pos = new Vector2(rect.X, rect.Y) + ext;
+
+			return new Rect(pos, ext);
+		}
+
+		/// <inheritdoc />
+		public static explicit operator ScreenRect(Rect rect)
+		{
+			Vector2 pos = rect.BottomLeft;
+
+			int x = HMath.RoundToInt(pos.X);
+			int y = HMath.RoundToInt(pos.Y);
+			int w = HMath.RoundToInt(rect.Extents.X * 2);
+			int h = HMath.RoundToInt(rect.Extents.Y * 2);
+
+			return new ScreenRect(x, y, w, h);
 		}
 
 		/// <inheritdoc />
---
+++ src/heng/_Shared/Rect.cs
@@ -0,0 +1,78 @@
+﻿namespace heng
+{
+	/// <summary>
+	/// Represents a 2-dimensional rectangular area in pixel-space.
+	/// <para>A <see cref="Rect"/> originates from its center point. Its <see cref="Extents"/> from
+	/// that center point then describe its dimensions.</para>
+	/// </summary>
+	public struct Rect
+	{
+		/// <summary>
+		/// The pixel-space center of the rect.
+		/// </summary>
+		public readonly Vector2 Center;
+
+		/// <summary>
+		/// The pixel-space extents of the rect.
+		/// <para>These are half of its width/height.</para>
+		/// </summary>
+		public readonly Vector2 Extents;
+
+		/// <summary>
+		/// The pixel-space size of the rect.
+		/// <para>This is its full width/height.</para>
+		/// </summary>
+		public Vector2 Size => Extents * 2;
+
+		/// <summary>
+		/// The pixel-space position of the rect's left edge.
+		/// </summary>
+		public Vector2 Left => Center - new Vector2(Extents.X, 0);
+
+		/// <summary>
+		/// The pixel-space position of the rect's right edge.
+		/// </summary>
+		public Vector2 Right => Center + new Vector2(Extents.X, 0);
+
+		/// <summary>
+		/// The pixel-space position of the rect's bottom edge.
+		/// </summary>
+		public Vector2 Bottom => Center - new Vector2(0, Extents.Y);
+
+		/// <summary>
+		/// The pixel-space position of the rect's top edge.
+		/// </summary>
+		public Vector2 Top => Center + new Vector2(0, Extents.Y);
+
+		/// <summary>
+		/// The pixel-space position of the rect's bottom-left corner.
+		/// </summary>
+		public Vector2 BottomLeft => Center - Extents;
+
+		/// <summary>
+		/// The pixel-space position of the rect's top-left corner.
+		/// </summary>
+		public Vector2 TopLeft => Center + new Vector2(-Extents.X, Extents.Y);
+
+		/// <summary>
+		/// The pixel-space position of the rect's top-right corner.
+		/// </summary>
+		public Vector2 TopRight => Center + Extents;
+
+		/// <summary>
+		/// The pixel-space position of the rect's bottom-right corner.
+		/// </summary>
+		public Vector2 BottomRight => Center + new Vector2(Extents.X, -Extents.Y);
+
+		/// <summary>
+		/// Constructs a new rect.
+		/// </summary>
+		/// <param name="center">The center origin of the rect, in pixel-space.</param>
+		/// <param name="extents">The extents of the rect, in pixel-space.</param>
+		public Rect(Vector2 center, Vector2 extents)
+		{
+			Center = center;
+			Extents = extents;
+		}
+	};
+}
